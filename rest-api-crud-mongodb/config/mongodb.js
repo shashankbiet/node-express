f72d7742404a5cef044bb81404a9d68288081582
shashankbiet <kesharwanishashank@gmail.com>
2020-07-07 21:22:15 +0530

    Adding unit test of student service

--- rest-api-crud-mongodb/config/mongodb.js
+++ rest-api-crud-mongodb/config/mongodb.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+let dbUrl;
 
+switch (process.env.ENVIRONMENT) {
+    case 'dev':
+        dbUrl = process.env.DEV_MONGO_URL;
+        break;
+
+    case 'test':
+        dbUrl = process.env.TEST_MONGO_URL;
+        break;
+}
 module.exports = () => {
-    mongoose.connect(process.env.DB_URL,
+    mongoose.connect(dbUrl,
         { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('MongoDB connected'))
         .catch(err => console.log(err));
